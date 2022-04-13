961c044462b260e21fc107baf16dd9427540ab36
muskan-gupta-0803 <guptamuskan662@gmail.com>
2022-04-13 15:03:28 +0530

    created header nad footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import "../Footer/Footer.css"
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <p className="footer-text">&copy; 2022 Style store | Made by Muskan Gupta</p>
+        </footer>
+    )
+}
