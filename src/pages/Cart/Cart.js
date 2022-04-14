477c2b49f6bd436e4819a78e635cf7fde22af4d8
muskan-gupta-0803 <guptamuskan662@gmail.com>
2022-04-14 15:24:39 +0530

    Added product, wishlist, cart pages

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,7 @@
+export function Cart(){
+    return(
+        <h1>
+            Your cart items will be listed here.
+        </h1>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Homepage } from "./Homepage/Homepage";
+export { ProductPage } from "./Products/ProductPage";
+export { Cart } from "./Cart/Cart";
+export { Wishlist } from "./Wishlist/Wishlist";
